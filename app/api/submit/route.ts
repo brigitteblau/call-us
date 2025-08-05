16ecf771203c701cf2794ef9db7a666dd3123ec0
Brigitte Blau <brigitteyaelblau@gmail.com>
2025-08-05 08:16:16 -0300

    req : request

--- app/api/submit/route.ts
+++ app/api/submit/route.ts
@@ -1,7 +1,7 @@
 import { prisma  } from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
-export async function POST(req) {
+export async function POST(req: Request) {
     const body = await req.json()
      
     try{
